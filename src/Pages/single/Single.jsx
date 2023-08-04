f8ffd5d74d2ec48eb96aba767aaf6d47023b93f2
susan <odiisusan@gmail.com>
2023-08-04 14:41:27 +0100

    complited single page ui

--- src/Pages/single/Single.jsx
+++ src/Pages/single/Single.jsx
@@ -1,14 +1,10 @@
 import './single.scss'
-import Navbar from '../../components/navbar/Navbar'
-import Sidebar from '../../components/sidebar/Sidebar'
 
 import Chart from '../../charts/Chart'
 import List from '../../components/table/Table'
-
-
-
-
+import Navbar from '../../components/navbar/Navbar'
 import React from 'react'
+import Sidebar from '../../components/sidebar/Sidebar'
 
 const Single = () => {
   return (
@@ -23,17 +19,17 @@ const Single = () => {
 <h1 className='title'> Information</h1>
 <div className='item'> 
 <img
-                src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
-                alt=""
-                className="itemImg"
-              />
-              <div className='details'>
-                <h1 className='itemmTitle'>  Peace John</h1>
-             <div className='detailItem'>
-              <span className='itemKey'> Email:</span>
-              <span className='itemValue'> emeka@gmail.com</span>
+    src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+    alt=""
+      className="itemImg"
+       />
+        <div className='details'>
+      <h1 className='itemmTitle'>  Peace John</h1>
+      <div className='detailItem'>
+      <span className='itemKey'> Email:</span>
+      <span className='itemValue'> emeka@gmail.com</span>
 
-             </div>
+       </div>
 
              <div className='detailItem'>
               <span className='itemKey'> Phone:</span>
@@ -60,10 +56,14 @@ const Single = () => {
 
   <Chart aspect={3/1} title = "User Spending {Last 6 Month}"/>
 </div>
+
+
 </div>
-<div className='bottoom'></div>
-<h1 className='title'> Last Transactions</h1>
+<div className='bottom'>
+<h1 className='title'>Last Transactions</h1>
 <List/>
+</div>
+
 </div>
 
 
